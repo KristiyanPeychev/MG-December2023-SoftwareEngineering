7f1402ea4667dd0f74036c3ee69f2ff0dec256b3
KristiyanPeychev <133945115+KristiyanPeychev@users.noreply.github.com>
2023-12-04 08:36:10 +0200

    Update File4.cs

--- File4.cs
+++ File4.cs
@@ -3,3 +3,9 @@ public class Country
     public string CountryName { get; set; }
     public string Capital { get; set; }
 }
+
+public class Bulgaria: Country
+{
+    public decimal GDP { get; set; }
+}
+
